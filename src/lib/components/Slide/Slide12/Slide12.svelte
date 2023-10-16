9e4cd53c473612f75e996412fdeb70cc6cbcb6e6
KevinITHS <kevin.ali@iths.se>
2023-10-16 22:41:01 +0200

    presentation klar

---
+++ src/lib/components/Slide/Slide12/Slide12.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Image from "./struktur1.jpg"
+	import Image2 from "./struktur2.jpg"
+	import Image3 from "./struktur3.jpg"
+</script>
+
+<div class="grid-stack">
+	<img src={Image} alt="struktur1" width="300px" />
+	<img src={Image2} alt="struktur2" width="300px" />
+	<img src={Image3} alt="struktur3" width="450px" />
+</div>
+
+<!-- 
+Aa, här ser ni lite hur strukturen för storybook ser ut, första och andra bilden är varje "story" som kommer dyka upp i 
+Storybooks integrerade sidebar. Och sen lägger man in ett namn under varje argument och i en separat .Svelte fil så gör man
+en if statement med argument namnet och skriver koden som är relevant för just den storyn där.
+Att använda Storybook ihop med Svelte känns typ lite flummigt, men jag tror att Storybook mest är gjord för att skapa 
+komponenter med hjälp av React eller Vue.
+-->
+
+<style>
+	div {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		max-width: 100vw;
+		max-height: 100vw;
+	}
+
+	img:nth-child(1) {
+		place-self: center start;
+		margin-left: 60px;
+	}
+	img:nth-child(2) {
+		place-self: center center;
+		margin-right: 200px;
+	}
+	img:nth-child(3) {
+		place-self: center end;
+		margin-right: 60px;
+	}
+</style>
--- src/lib/components/Slide/Slide8/Slide8.svelte
+++ src/lib/components/Slide/Slide8/Slide8.svelte
@@ -6,7 +6,7 @@
 	<img src={Image} alt="whysvelte" width="600px" />
 </div>
 
-<!-- EIGHT
+<!-- EIGHT 
 SvelteKit är känd för sin snabbhet. Ramverket optimerar koden så att den tar
 mindre utrymme och körs snabbare, som ger en bättre användarupplevelse. Detta är särskilt bra för applikationer
 som kräver mycket client-side scripting och rendering. Den är också lättare att lära sig jämfört med React, om man kommer in
